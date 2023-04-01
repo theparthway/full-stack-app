949fee0bee306b65ce2834055ac835788f4876bc
theparthway <parthpsh@gmail.com>
2023-04-02 04:48:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
 // connect to database
 connectToDb();
